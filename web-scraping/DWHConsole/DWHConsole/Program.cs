0c0f819306d78c9f2174fbee211f8c176b4bb64a
ZoobiDoobi <bscsf12m033@gmail.com>
2015-11-03 01:28:34 +0500

    Change In Program.cs

--- web-scraping/DWHConsole/DWHConsole/Program.cs
+++ web-scraping/DWHConsole/DWHConsole/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
-using System.IO;
 using System.Data;
-using System.Data.SqlClient;
 using HtmlAgilityPack;
 
 namespace DWHConsole
